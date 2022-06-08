d4a49131552896179575f06c3b3f4fad9e8e321a
Onepo1ntFive <name.diesel@gmail.com>
2022-06-08 12:14:33 +0300

    Some magic with hightcharts

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-
-// Highcharts.getJSON('https://demo-live-data.highcharts.com/aapl-c.json', function (data) {
-
-//     console.log('AAPL', data)
-//     // Create the chart
-//     Highcharts.stockChart('container', {
-//         rangeSelector: {
-//             selected: 2
-//         },
-
-//         title: {
-//             text: 'AAPL Stock Price'
-//         },
-
-//         series: [{
-//             name: 'AAPL',
-//             data: data,
-//             tooltip: false
-//         }]
-//     });
-// });
-
-// ---------------------------------------------------------------------------------
-
 const chartBlock = document.querySelector('.js-chart');
 const loaderBlock = document.querySelector('.js-loader');
 
@@ -31,15 +7,25 @@ let fullData = [];
 let dateFrom = null;
 let dateTill = null;
 
-let board = 'tqbr';
-let total = null;
+let board = 'TQBR';
 let page = 0;
 let itemsPerPage = 100;
 let totalItems = null;
 let pages = null;
 
-let queryParams = new URLSearchParams();
 // 
+Highcharts.setOptions({
+    lang: {
+        months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        shortMonths: ['янв', 'фев', 'мар', 'апр', 'мая', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'],
+        weekdays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+        shortWeekdays: ['Вc', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        rangeSelectorZoom: '',
+    }
+});
+
+// 
+let queryParams = new URLSearchParams();
 let params = null;
 let defaultParams = {
     'iss.meta': 'off',
@@ -51,17 +37,9 @@ for (let key in defaultParams) {
 
 // get data dates range
 Highcharts.getJSON(`${src}NVTK/dates.json?${defaultParams}`, (data) => {
-    dateFrom = data.dates.data[0];
-    dateTill = data.dates.data[1];
+    dateFrom = data.dates.data[0][0];
+    dateTill = data.dates.data[0][1];
 });
-
-dateParams = {
-    'from': dateFrom,
-    'till': dateTill,
-}
-for (let key in dateParams) {
-    queryParams.append(key, dateParams[key]);
-}
 
 axios
     .get(`${src}NVTK.json`)
@@ -82,26 +60,55 @@ async function getDataByPage(page) {
             }
 
             if (totalItems > fullData.length) {
+                // get all data page by page
                 getDataByPage(nextPage);
             } else {
+                // if data all - draw chart
                 drawHighchart(transformDataForHighcharts(fullData));
             }
         })
 }
-
 getDataByPage(page);
 
+// 
 function transformDataForHighcharts(data) {
-    let transformedData = [];
-    for (const item of data) {
-        if (!!item[11]) {
-            transformedData.push(
-                [
-                    // date col
-                    new Date(item[1]).getTime(),
-                    // cost
-                    item[11]
-                ]
+    let transformedData = {};
+    transformedData.data = [];
+    transformedData.volume = [];
+
+    //get items by board
+    let filteredData = data.filter(item => item[0] === board);
+
+    for (const [index, item] of filteredData.entries()) {
+        if (!!item[6]) {
+            // costs
+            transformedData.data.push(
+                {
+                    x: new Date(item[1]).getTime(),
+                    y: item[13],
+                    open: item[6],
+                    close: item[11],
+                    high: item[8],
+                    low: item[7],
+                    cost: item[13],
+                }
+            )
+
+            // volumes
+            let prevIndex = (index > 0) ? index - 1 : index;
+            let volume = filteredData[index][13];
+            let prevVolume = filteredData[prevIndex][13];
+
+            let green = 'rgb(36, 132, 123)'; // green
+            let red = 'rgb(209, 66, 66)'; // red
+            let colColor = (volume > prevVolume) ? green : red;
+
+            transformedData.volume.push(
+                {
+                    x: new Date(item[1]).getTime(),
+                    y: item[12],
+                    color: colColor
+                }
             )
         }
     }
@@ -112,15 +119,138 @@ function transformDataForHighcharts(data) {
 function drawHighchart(data) {
     loaderBlock.classList.remove('active');
     chartBlock.classList.add('active');
+
     Highcharts.stockChart('chart', {
         rangeSelector: {
-            selected: 2
+            selected: 2,
+            buttons: [{
+                type: 'month',
+                count: 1,
+                text: 'Месяц',
+                title: 'Просмотреть 1 месяц',
+            }, {
+                type: 'month',
+                count: 3,
+                text: 'Квартал',
+                title: 'Просмотреть 3 месяца',
+            }, {
+                type: 'month',
+                count: 6,
+                text: 'Пол года',
+                title: 'Просмотреть 6 месяцев',
+            }, {
+                type: 'year',
+                count: 1,
+                text: 'Год',
+                title: 'Просмотреть 1 год',
+            }, {
+                type: 'all',
+                text: 'Всё время',
+                title: 'За всё время',
+                width: 0
+            }],
+            buttonTheme: {
+                paddingLeft: 10,
+                paddingTop: 5,
+                paddingRight: 10,
+                paddingbottom: 5,
+                width: 'auto',
+            }
         },
 
-        series: [{
-            name: 'NVTK',
-            data: data,
-            tooltip: false
-        }]
+        exporting: {
+            enabled: false,
+        },
+
+        tooltip: {
+            shape: 'square',
+            headerShape: 'callout',
+            borderWidth: 0,
+            shadow: false,
+            positioner: function (width, height, point) {
+                let chart = this.chart;
+                let position;
+
+                if (point.isHeader) {
+                    position = {
+                        x: Math.max(
+                            chart.plotLeft,
+                            Math.min(
+                                point.plotX + chart.plotLeft - width / 2,
+                                chart.chartWidth - width - chart.marginRight
+                            )
+                        ),
+                        y: point.plotY
+                    };
+                } else {
+                    position = {
+                        x: point.series.chart.plotLeft,
+                        y: point.series.yAxis.top - chart.plotTop
+                    };
+                }
+
+                return position;
+            }
+        },
+
+        yAxis: [
+            {
+                title: {
+                    text: 'NVTK'
+                },
+                height: '80%',
+                lineWidth: 1,
+                resize: {
+                    enabled: true
+                },
+            },
+            {
+                title: {
+                    text: 'Volume'
+                },
+                top: '80%',
+                height: '20%',
+                offset: 0,
+                lineWidth: 1,
+                resize: {
+                    enabled: true
+                },
+            }
+        ],
+
+        series: [
+            // shares cost line
+            {
+                type: 'line',
+                name: 'NVTK',
+                data: data.data,
+                turboThreshold: 5000,
+                tooltip: {
+                    pointFormatter: function () {
+                        let text = `
+                            <div> <b>Открытие</b>: ${Highcharts.numberFormat(this.open, 0)} </div>
+                            <div> <b>Закрытие</b>: ${Highcharts.numberFormat(this.close, 0)} </div>
+                            <div> <b>Max</b>: ${Highcharts.numberFormat(this.high, 0)} </div>
+                            <div> <b>Min</b>: ${Highcharts.numberFormat(this.low, 0)} </div>
+                        `
+                        return text;
+                    }
+                },
+            },
+            // volumes graph
+            {
+                type: 'column',
+                name: 'Volume',
+                data: data.volume,
+                turboThreshold: 0,
+                yAxis: 1,
+                pointWidth: 1,
+                tooltip: {
+                    pointFormatter: function () {
+                        return `<b>Объём</b> (акции): ${Highcharts.numberFormat(this.y, 0)}`;
+                    }
+                },
+            }
+        ]
     });
 }
