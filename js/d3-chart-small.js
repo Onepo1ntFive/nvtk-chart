45f18466e67ee172ce121178f61fd2cf0793d651
Onepo1ntFive <name.diesel@gmail.com>
2022-06-20 16:45:16 +0300

    Small chart, absolute pos for legend

--- js/d3-chart-small.js
+++ js/d3-chart-small.js
@@ -12,7 +12,7 @@
 
     // first time get 1 year data
     yearToDate = new Date();
-    yearToDate.setMonth(yearToDate.getMonth() - 12);
+    yearToDate.setMonth(yearToDate.getMonth() - 3);
     yearToDate = yearToDate.toLocaleDateString('fr-CA');
     dateQueryParam = `&from=${yearToDate}`;
 
@@ -61,19 +61,6 @@
                 }
             })
     }
-
-    document.querySelector('.js-all').addEventListener('click', (e) => {
-        if (canGetFullData) {
-            e.target.classList.add('active')
-            loader.classList.add('active');
-            chartBlock.classList.remove('active');
-
-            dateQueryParam = '';
-            canGetFullData = false;
-            fullData = [];
-            getDataByPage(page);
-        }
-    })
     // -------------------------
     function initialiseChart(data) {
         data = data.filter(
@@ -82,14 +69,10 @@
             }
         );
         let svg = document.querySelector('#chart');
-        const margin = { top: 0, right: 50, bottom: 75, left: 10 };
-        const margin2 = { top: 500, right: 50, bottom: 50, left: 10 };
-        const margin3 = { top: 550, right: 50, bottom: 0, left: 10 };
+        const margin = { top: 10, right: 50, bottom: 30, left: 10 };
 
         const width = svg.offsetWidth - margin.left - margin.right;
         const height = svg.offsetHeight - margin.top - margin.bottom;
-        const height2 = svg.offsetHeight - margin2.top - margin2.bottom;
-        const height3 = svg.offsetHeight - margin3.top - margin3.bottom;
 
         // find data range
         let xMin = d3.min(data, d => {
@@ -114,29 +97,11 @@
         });
 
         let x = d3.scaleTime().domain([xMin, xMax]).range([0, width]);
-        let y = d3
-            .scaleLinear()
-            .domain([yMin, yMax])
-            .range([height - margin.top - margin.bottom, 0]);
+        let y = d3.scaleLinear().domain([yMin, yMax]).range([height, 0]);
 
-        let x2 = d3.scaleTime().domain([xMin, xMax]).range([0, width]);
-        let y2 = d3.scaleLinear().domain([yMin2, yMax2]).range([height2, 0]);
-
-        let x3 = d3.scaleTime().domain([xMin, xMax]).range([0, width]);
-        let y3 = d3.scaleLinear().domain([yMin, yMax]).range([height3, 0]);
 
         let xAxis = d3.axisBottom(x);
         let yAxis = d3.axisRight(y);
-
-        let brush = d3.brushX()
-            .extent([[0, 0], [width, height3]])
-            .on("start brush end", brushed);
-
-        let zoom = d3.zoom()
-            .scaleExtent([1, Infinity])
-            .translateExtent([[0, 0], [width, height]])
-            .extent([[0, 0], [width, height]])
-            .on("zoom", zoomed);
 
         // scale using range
         const xScale = d3
@@ -191,15 +156,6 @@
             // .call(d3.axisRight(yScale));
             .call(yAxis);
 
-        // clip for zoom
-        let clip = svg.append("defs").append("svg:clipPath")
-            .attr("id", "clip")
-            .append("svg:rect")
-            .attr("width", width + 3)
-            .attr("height", height)
-            .attr("x", 0)
-            .attr("y", 0);
-
         // main line
         const line = d3
             .line()
@@ -216,44 +172,9 @@
             .style('fill', 'none')
             .attr("class", "line")
             .attr('id', 'priceChart')
-            .attr('stroke', 'steelblue')
+            .attr('stroke', '#f00')
             .attr('stroke-width', '1')
-            .attr("clip-path", "url(#clip)")
             .attr('d', line);
-
-        // brush line
-        const line2 = d3
-            .line()
-            .x(d => {
-                return x3(d['TRADEDATE']);
-            })
-            .y(d => {
-                return y3(d['CLOSE']);
-            });
-
-        svg
-            .append('path')
-            .data([data])
-            .style('fill', 'none')
-            .attr('id', 'priceChart')
-            .attr('stroke', 'steelblue')
-            .attr('stroke-width', '1')
-            .attr('transform', `translate(0, ${margin3.top})`)
-            .attr('d', line2);
-
-        let zoom_line = svg.append("g")
-            .attr("class", "zoom_line")
-            .attr("transform", `translate(0, ${margin3.top})`);
-
-        // brush for zoom
-        zoom_line.append("g")
-            .attr("class", "brush")
-            .attr('stroke-width', '0')
-            .call(brush)
-            .call(brush.move, x.range());
-
-        d3.select('.selection')
-            .attr('fill', 'rgba(102, 133, 194, 0.3)')
 
         // y crosshair
         const focus = svg
@@ -263,15 +184,15 @@
 
         focus.append('line').classed('y', true);
         focus.append('circle')
-            .attr('r', 4)
+            .attr('r', 2)
             .attr('fill', 'rgb(255, 255, 255)')
-            .attr('stroke', 'rgb(124, 181, 236)')
+            .attr('stroke', 'rgb(255, 255, 255)')
             .attr('stroke-width', '10')
             .attr('stroke-opacity', '0.5');
         focus.append('circle')
             .attr('r', 2)
-            .attr('fill', 'rgb(124, 181, 236)')
-            .attr('stroke', 'rgb(124, 181, 236)')
+            .attr('fill', 'rgb(255, 255, 255)')
+            .attr('stroke', 'rgb(255, 255, 255)')
 
         // crosshair overlay
         svg
@@ -335,7 +256,7 @@
                 .attr('class', 'lineLegendBg')
                 .attr('width', 165)
                 .attr('height', 60)
-                .attr('fill', 'rgba(124, 181, 236, 0.2)')
+                .attr('fill', 'rgba(255, 255, 255, 0.7)')
                 .attr('transform', () => {
                     return `translate(${x(currentData['TRADEDATE']) + xPosBg}, ${y(currentData['CLOSE']) - yPosBg})`;
                 })
@@ -367,105 +288,6 @@
                 .attr('transform', `translate(${xPos}, ${-yPos})`); //align texts with boxes
         };
 
-        function drawColumns(data) {
-            const volData = data.filter(d => d['VOLUME'] !== null && d['VOLUME'] !== 0);
-
-            const yMinVolume = d3.min(volData, d => {
-                return Math.min(d['VOLUME']);
-            });
-            const yMaxVolume = d3.max(volData, d => {
-                return Math.max(d['VOLUME']);
-            });
-
-            let yVolumeScale = d3
-                .scaleLinear()
-                .domain([yMinVolume, yMaxVolume])
-                .range([height, height - margin.top - margin.bottom]);
-
-            // remove all before draw for zoom
-            svg.selectAll("rect.vol").remove();
-
-            // draw columns
-            svg
-                .selectAll()
-                .data(volData)
-                .enter()
-                .append('rect')
-                .attr('x', d => {
-                    return xScale(d['TRADEDATE']);
-                })
-                .attr('y', d => {
-                    return yVolumeScale(d['VOLUME']);
-                })
-                .attr('class', 'vol')
-                .attr("clip-path", "url(#clip)")
-                .attr('fill', (d, i) => {
-                    if (i === 0) {
-                        return '#03a678';
-                    } else {
-                        return volData[i - 1].CLOSE > d.CLOSE ? 'rgb(209, 66, 66)' : 'rgb(36, 132, 123)';
-                    }
-                })
-                .attr('width', 2)
-                .attr('height', d => {
-                    return height - yVolumeScale(d['VOLUME']);
-                });
-
-            // y axis for volumes
-            if (!document.querySelector('.vol-y')) {
-                svg.append('g')
-                    .attr('class', 'vol-y')
-                    .attr("transform", `translate(${width + 4}, 0)`)
-                    .call(d3.axisRight(yVolumeScale).ticks(2).tickFormat(x => `${nFormatter(x, 1)}`));
-            }
-        }
-
-        /* zoom by brush */
-        function brushed() {
-            if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
-            let s = d3.event.selection || x3.range();
-            x.domain(s.map(x3.invert, x3));
-            xScale.domain(s.map(x3.invert, x3));
-            svg.select(".line").attr("d", line);
-            drawColumns(data);
-            svg.select(".axis--x").call(xAxis);
-            svg.select(".zoom").call(zoom.transform, d3.zoomIdentity.scale(width / (s[1] - s[0])).translate(-s[0], 0));
-        }
-        function zoomed() {
-            if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
-            let t = d3.event.transform;
-            x.domain(t.rescaleX(x3).domain());
-            x2.domain(t.rescaleX(x3).domain());
-            svg.select(".line").attr("d", line);
-            svg.select(".axis--x").call(xAxis);
-            zoom_line.select(".brush").call(brush.move, x.range().map(t.invertX, t));
-        }
-
-        // time range selector
-        const timeSelectorBtns = document.querySelectorAll('.js-timeselect');
-        Array.prototype.forEach.call(timeSelectorBtns, function (timeSelectorBtn, i) {
-            if (!canGetFullData) {
-                timeSelectorBtn.disabled = true;
-                timeSelectorBtn.classList.remove('active');
-            }
-
-            timeSelectorBtn.addEventListener('click', (event) => {
-                let thisBtn = event.target;
-                Array.prototype.forEach.call(timeSelectorBtns, function (el, i) {
-                    el.classList.remove('active');
-                });
-                thisBtn.classList.add('active');
-                let range = timeSelectorBtn.dataset.range;
-                d3.select(".brush").transition().duration(1000).call(brush.move, [width - width / (12 / range), width]);
-            })
-        });
-
-        // set brush on time interval changes
-        if (canGetFullData) {
-            d3.select(".brush").transition().duration(1000).call(brush.move, [width - width / 12, width]);
-        } else {
-            d3.select(".brush").transition().duration(3000).call(brush.move, [0, width]);
-        }
 
     }; // end chart 
 
@@ -488,22 +310,6 @@
 
     };
     // -------------------------
-    function nFormatter(num, digits) {
-        const lookup = [
-            { value: 1, symbol: "" },
-            { value: 1e3, symbol: "k" },
-            { value: 1e6, symbol: "M" },
-            { value: 1e9, symbol: "G" },
-            { value: 1e12, symbol: "T" },
-            { value: 1e15, symbol: "P" },
-            { value: 1e18, symbol: "E" }
-        ];
-        const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-        let item = lookup.slice().reverse().find(function (item) {
-            return num >= item.value;
-        });
-        return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
-    }
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
